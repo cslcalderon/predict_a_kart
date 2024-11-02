8da0c2379298003f828e6a1c657d427c073aa1a9
KristiPhammie <143444205+KristiPhammie@users.noreply.github.com>
2024-11-02 14:36:03 -0500

    made character class

---
+++ mario_model.py
@@ -0,0 +1,43 @@
+class Character():
+    def __init__ (self):
+      self.weight = weight
+      self.acceleration = acceleration
+      self.top_speed = top_speed
+      self.handling = handling
+      self.choco_island = ci
+      self.vanilla_lake = vl
+      self.donut_plains = dp
+      self.koopa_beach = kb
+      self.rainbow_road = = rr
+      
+    def get_weight(self):
+      return self.weight
+
+    def get_accelation(self):
+      return self.acceleration
+
+    def get_top_speed(self):
+      return self.top_speed
+      
+    def get_handling(self):
+      return self.handling
+      
+    def get_ci(self):
+      return self.choco_island
+      
+    def get_vl(self):
+      return self.vanilla_lake
+      
+    def get_dp(self):
+      return self.donut_plains
+      
+    def get_kp(self):
+      return self.koopa_beach
+      
+    def get_rr(self):
+      return self.rainbow_road
+
+    def __str__(self):
+      return str(self.weight)
+
+      
