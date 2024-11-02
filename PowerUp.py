3d202218ce7ddd42ad1affed2aab39792dabc143
Kristi Pham <sweetmama909@wireless-10-146-171-176.public.utexas.edu>
2024-11-02 15:00:03 -0500

    update power up file

---
+++ PowerUp.py
@@ -0,0 +1,27 @@
+class PowerUp():
+    def __init__(self, ci, vl, dp, kb, rr, speed_related):
+        self.ci = ci
+        self.vl = vl
+        self.dp = dp
+        self.kb = kb
+        self.rr = rr
+        self.speed_related = speed_related
+    
+    def get_ci(self):
+        return self.ci
+    
+    def get_vl(self):
+        return self.vl
+    
+    def get_dp(self):
+        return self.dp
+    
+    def get_kb(self):
+        return self.kb
+    
+    def get_rr(self):
+        return self.rr
+    
+    def get_speed_related(self):
+        return self.speed_related
+        
